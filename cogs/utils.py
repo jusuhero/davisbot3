fda54d450122cc7d0181d545f39778ac03292608
Justin Ackermann <75494762+jusuhero@users.noreply.github.com>
2024-11-11 01:22:13 +0100

    Basic help function and making Davisbot a Class (#2)
    * Changed davisbot to a class based structure
    
    * Disable default help command
    
    * Feature: cogs/utils - Added helping help function to help users
    
    * Comments: cogs/utils - Added comments to help function

--- cogs/utils.py
+++ cogs/utils.py
@@ -1,20 +1,77 @@
+import discord
 from discord.ext import commands
+from discord.ui import View, Select
+from itertools import chain
+
+
+class HelpSelect(Select):
+    # Selection to let the user select a cog first to see help for.
+    def __init__(self, bot):
+        super().__init__(
+            placeholder="Choose a category",
+            options=[
+                discord.SelectOption(label=cog_name, description=cog.__doc__)
+                for cog_name, cog in bot.cogs.items()  # Iterate over all available cogs and let the user select between them
+            ],
+        )
+
+        self.bot = bot
+
+    async def callback(self, interaction: discord.Interaction):
+        try:
+            # Gets the Cog that has been currently selected in the HelpSelect Select.
+            cog = self.bot.get_cog(self.values[0])
+            assert cog
+
+            # Gathers list of commands (normal and app_commands) - itertools.chain is used to generate one iterator instead of two for loops
+            commands_list = list(chain(cog.walk_commands(), cog.walk_app_commands()))
+
+            # Send Embed of the available Commands in the selected cog.
+            embed = discord.Embed(
+                title=f"{cog.__cog_name__} Commands",
+                description="\n".join(
+                    f"• **{command.name}**: `{command.description or 'No Description provided'}`"
+                    for command in commands_list
+                ),
+                color=discord.Color.blue(),
+            )
+            await interaction.response.edit_message(embed=embed)
+
+        except AssertionError as ae:
+            print(
+                f"Could not load help info for cog {cog}. Does it exist? Error: {repr(ae)}"
+            )
+
 
 class UtilsCog(commands.Cog, description="Provides utility and information functions."):
     def __init__(self, bot):
         self.bot = bot
-    
+
     @commands.hybrid_command(name="ping")
     async def ping(self, ctx: commands.Context):
         await ctx.send(f"> Pong! {round(self.bot.latency * 1000)}ms")
+
+    @commands.hybrid_command(
+        name="help",
+        description="Shows a help section with a list of commands available in all cogs..",
+    )
+    async def help_function(self, ctx: commands.Context):
+        embed = discord.Embed(
+            title=f"Davisbot v{self.bot.VERSION} - Help section",
+            description="Select an entry from the dropdown menu below to see the available commands of the respective module. ",
+        )
+        view = View().add_item(HelpSelect(self.bot))
+        await ctx.send(embed=embed, view=view, ephemeral=True)
 
     @commands.command(name="refreshslashcommands")
     async def refresh_slashcommands(self, ctx: commands.Context):
         await self.bot.tree.sync()
         await ctx.send("Commands refreshed. Restart you client.", ephemeral=True)
 
+
 async def setup(bot):
     await bot.add_cog(UtilsCog(bot))
+
 
 async def teardown():
     print("Utils Extension unloaded!")
--- davisbot.py
+++ davisbot.py
@@ -4,42 +4,47 @@ import os
 
 DAVISBOT_TOKEN = os.getenv("DAVISBOT_TOKEN")
 
-# Set up bot intents
 intents = discord.Intents.default()
 intents.message_content = True  # Required for reading message content
 intents.guilds = True
 intents.guild_messages = True
 intents.reactions = True  # Required for tracking reactions
-intents.guild_reactions = True  # For assigning roles via reactions
+intents.guild_reactions = True  # For assigning roles via rea
 
-# Initialize the bot
-bot = commands.Bot(command_prefix=".", intents=intents)
 
-# Load cogs
-initial_extensions = [
-    "cogs.music",
-    "cogs.advent_of_code",
-    "cogs.utils"
-]
+class Davisbot(commands.Bot):
 
-async def setup_hook() -> None:
-    for extension in initial_extensions:
-        await bot.load_extension(extension)
+    # Available Cogs
+    initial_extensions = ["cogs.music", "cogs.advent_of_code", "cogs.utils"]
 
-@bot.event
-async def on_ready() -> None:
-    print("---------------------------")
-    print("Logged in")
-    print("Username: ", end="")
-    print(bot.user.name)
-    print("Userid: ", end="")
-    print(bot.user.id)
-    await bot.change_presence(
-        status=discord.Status.dnd, activity=discord.Game("1&1 Kundenservice")
-    )
-    print("---------------------------")
+    def __init__(self, intents: discord.Intents, **kwargs):
+        super().__init__(
+            intents=intents, command_prefix=commands.when_mentioned_or(".")
+        )
+
+    async def setup_hook(self) -> None:
+        for extension in self.initial_extensions:
+            try:
+                await self.load_extension(extension)
+            except Exception as e:
+                print(
+                    f"Could not load extension {extension} due to {e.__class__.__name__}: {repr(e)}"
+                )
+
+    async def on_ready(self) -> None:
+        print("---------------------------")
+        print("Logged in")
+        print("Username: ", end="")
+        print(self.user.name)
+        print("Userid: ", end="")
+        print(self.user.id)
+        await self.change_presence(
+            status=discord.Status.dnd, activity=discord.Game("1&1 Kundenservice")
+        )
+        await self.tree.sync()
+        print("---------------------------")
 
 
 if __name__ == "__main__":
-    bot.setup_hook = setup_hook
+    bot = Davisbot(intents=intents, help_command=None)
     bot.run(DAVISBOT_TOKEN)
